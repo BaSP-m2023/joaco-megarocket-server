f9dc455f310951d2d360aa0865d07dd57dcd1e5b
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2023-05-05 16:36:18 -0300

    MR: Delete member addedd

--- src/resources/member.js
+++ src/resources/member.js
@@ -13,4 +13,17 @@ router.get('/:id', (req, res) => {
   const memberFound = members.find((member) => member.id.toString() === id);
   (memberFound) ? res.send(memberFound) : res.send('Member not exist');
 });
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const filterMembers = members.filter((member) => member.id.toString() !== id);
+  fs.writeFile(
+    'src/data/member.json',
+    JSON.stringify(filterMembers, null),
+    (err) => {
+      err ? res.send('Member cannot deleted') : res.send('Member Deleted!');
+    },
+  );
+});
+
 module.exports = router;
