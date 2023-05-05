9a327c99b07ef988a0a91ca2607ea13f7a219f4c
JulianRiedinger7 <julianriedinger27@gmail.com>
2023-05-05 18:35:43 -0300

    MR: created POST request to create admin

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,4 +1,4 @@
-// const fs = require('fs');
+const fs = require('fs');
 const express = require('express');
 const admins = require('../data/admins.json');
 
@@ -8,6 +8,22 @@ router.get('/', (req, res) => {
   res.status(200).json({
     data: admins,
   });
+});
+
+router.post('/', (req, res) => {
+  const newAdmin = req.body;
+  if (Object.keys(newAdmin).length > 0) {
+    admins.push(newAdmin);
+    fs.writeFile('src/data/admins.json', JSON.stringify(admins, null, 2), (err) => {
+      if (err) {
+        res.status(400).json({ msg: 'Error! Admin cannot be created' });
+      } else {
+        res.status(200).json({ msg: 'Admin created succesfully!' });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: 'Admin info is required' });
+  }
 });
 
 module.exports = router;
