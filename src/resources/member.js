49ab36f670b645bac4a5992dbe948c72ed6e0559
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2023-05-05 17:34:12 -0300

    MR: Create User added

--- src/resources/member.js
+++ src/resources/member.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const router = express.Router();
 const members = require('../data/member.json');
 
+router.use(express.urlencoded({ extended: false }));
+
 router.get('/', (req, res) => {
   res.send(members);
 });
@@ -24,6 +26,24 @@ router.delete('/:id', (req, res) => {
       err ? res.send('Member cannot deleted') : res.send('Member Deleted!');
     },
   );
+});
+
+router.post('/', (req, res) => {
+  const member = req.body;
+  members.push(member);
+  res.send(`Usuario agregado: ${JSON.stringify(member.name)}`);
+});
+
+router.post('/', (req, res) => {
+  const member = req.body;
+  fs.readFile('src/data/member.json', (err) => {
+    if (err) throw err;
+    members.push(member);
+    fs.writeFile('src/data/member.json', JSON.stringify(members), (err) => {
+      if (err) throw err;
+      res.send(`Usuario agregado: ${JSON.stringify(member.name)}`);
+    });
+  });
 });
 
 module.exports = router;
