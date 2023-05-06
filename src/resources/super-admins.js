bfcb7f3cadc3d4069220ba2fa3df7ad3f932a70f
Alejandro <victoriaecalle@gmail.com>
2023-05-06 18:37:16 -0300

    MR: Added router for PUT superadmin

--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const fs = require('fs');
-const superadmin = require('../data/super-admins.json');
+const superadmins = require('../data/super-admins.json');
 
 const router = express.Router();
-router.get('/get', (req, res) => {
-  res.send(superadmin);
+
+router.get('/', (req, res) => {
+  res.send(superadmins);
 });
 
-router.get('/getById/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const userId = req.params.id;
-  const found = superadmin.find((user) => user.id === userId);
+  const found = superadmins.find((user) => user.id === userId);
   if (found) {
     res.send(found);
   } else {
@@ -17,10 +18,10 @@ router.get('/getById/:id', (req, res) => {
   }
 });
 
-router.post('/post', (req, res) => {
+router.post('/', (req, res) => {
   const newSuperadmin = req.body;
-  superadmin.push(newSuperadmin);
-  fs.writeFile('src/data/super-admins.json', JSON.stringify(superadmin, null, 2), (err) => {
+  superadmins.push(newSuperadmin);
+  fs.writeFile('src/data/super-admins.json', JSON.stringify(superadmins, null, 2), (err) => {
     if (err) {
       res.send('Superadmin cannot be created');
     } else {
@@ -31,7 +32,7 @@ router.post('/post', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   const superadminId = req.params.id;
-  const filteredUsers = superadmin.filter((user) => user.id !== superadminId);
+  const filteredUsers = superadmins.filter((user) => user.id !== superadminId);
   fs.writeFile('src/data/super-admins.json', JSON.stringify(filteredUsers, null, 2), (err) => {
     if (err) {
       res.send('Superadmin cannot be deleted');
@@ -39,6 +40,21 @@ router.delete('/:id', (req, res) => {
       res.send('Superadmin deleted');
     }
   });
+});
+
+router.put('/:id', (req, res) => {
+  const superadminId = req.params.id;
+  const found = superadmins.find((superadmin) => superadmin.id === superadminId);
+  const updSuperadmin = req.body;
+  if (found) {
+    found.first_name = updSuperadmin.first_name ? updSuperadmin.first_name : found.first_name;
+    found.last_name = updSuperadmin.last_name ? updSuperadmin.last_name : found.last_name;
+    found.email = updSuperadmin.email ? updSuperadmin.email : found.email;
+    found.password = updSuperadmin.password ? updSuperadmin.password : found.password;
+    res.send(found);
+  } else {
+    res.send('A superadmin with that ID doesnt exist');
+  }
 });
 
 module.exports = router;
