02ef42dbba0064897131c38aba7fd41267412c7f
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-05-06 17:29:13 -0300

    MR: POST request finished with validations

--- src/resources/member.js
+++ src/resources/member.js
@@ -11,6 +11,7 @@ router.put('/:id', (req, res) => {
     const updateMember = req.body;
     members.forEach((member) => {
       if (member.id.toString() === req.params.id) {
+        /* eslint-disable no-param-reassign */
         member.first_name = updateMember.first_name || member.first_name;
         member.last_name = updateMember.last_name || member.last_name;
         member.address = updateMember.address || member.address;
@@ -24,7 +25,51 @@ router.put('/:id', (req, res) => {
       }
     });
   } else {
-    res.status(400).json({ message: `The member with the ID: ${req.params.id}` });
+    res.status(400).json({ message: `The member with the ID:${req.params.id} doesn't exist` });
+  }
+});
+
+router.post('/', (req, res) => {
+  let newMember = req.body;
+  const lastId = members[members.length - 1].id;
+  const condition = Object.values(newMember).every((atribute) => atribute !== '');
+  const memberKeys = {
+    first_name: '',
+    last_name: '',
+    address: '',
+    date: '',
+    phone: '',
+    email: '',
+    password: '',
+    membership: '',
+  };
+
+  const actualKeys = Object.keys(memberKeys);
+  const newKeys = Object.keys(newMember);
+
+  let equalKeys = true;
+  for (let i = 0; i < actualKeys.length; i += 1) {
+    if (actualKeys[i] !== newKeys[i]) {
+      equalKeys = false;
+    }
+  }
+
+  newMember = { id: lastId + 1, ...newMember };
+
+  if (Object.entries(newMember).length === 0 || !condition || !equalKeys) {
+    res.status(400).json({ message: 'You must complete all the properties:', properties: `${Object.keys(memberKeys)}` });
+  } else {
+    members.push(newMember);
+    fs.writeFile('src/data/member.json', JSON.stringify(members, null, 2), (err) => {
+      if (err) throw err;
+      res.status(200).json({ message: 'User added successfuly!', members });
+    });
+  }
+
+  const memberExists = members.some((element) => element.email === newMember.email);
+
+  if (memberExists) {
+    res.status(400).json({ message: 'This member already exists' });
   }
 });
 
