ca2538b6f59986aa3fec7cfe818257bc6f84384c
YhanahiRosenthal <yhanahi16@gmail.com>
2023-05-06 23:16:05 -0300

    MR: method put created, some admins modified

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,4 +1,4 @@
-// const fs = require('fs');
+const fs = require('fs');
 const express = require('express');
 const admins = require('../data/admins.json');
 
@@ -18,6 +18,33 @@ router.get('/:id', (req, res) => {
     res.status(200).json(getAdmin);
   } else {
     res.status(400).json({ error: 'admin not found' });
+  }
+});
+
+router.put('/edit/:id', (req, res) => {
+  const adminId = req.params.id;
+  const foundAdmin = admins.find((admin) => admin.id === adminId);
+
+  if (foundAdmin) {
+    const {
+      id, email, password, firstName, lastName, dni, phone,
+    } = req.body;
+
+    if (id && id !== foundAdmin.id) {
+      res.status(400).json({ msg: 'Id can not be modified' });
+    }
+    foundAdmin.email = email || foundAdmin.email;
+    foundAdmin.password = password || foundAdmin.password;
+    foundAdmin.firstName = firstName || foundAdmin.firstName;
+    foundAdmin.lastName = lastName || foundAdmin.lastName;
+    foundAdmin.dni = dni || foundAdmin.dni;
+    foundAdmin.phone = phone || foundAdmin.phone;
+
+    fs.writeFileSync('src/data/admins.json', JSON.stringify(admins));
+
+    res.status(200).json({ msg: 'Admin updated successfully' });
+  } else {
+    res.status(400).json({ msg: 'Admin not found' });
   }
 });
 
