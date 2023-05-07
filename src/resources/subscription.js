b24b95c29adf4aa0037494f550d27bdd88ff06bc
Eugenio Weskamp <e.weskamp8@gmail.com>
2023-05-07 15:50:02 -0300

    MR: Update GET, POST, DELETE subscription.js

--- src/resources/subscription.js
+++ src/resources/subscription.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const fs = require('fs');
 
 const router = express.Router();
 const subscriptions = require('../data/subscription.json');
 
 router.use(express.urlencoded({ extended: false }));
+
+// GET
 
 router.get('/', (req, res) => {
   res.send(subscriptions);
@@ -14,7 +17,37 @@ router.get('/:id', (req, res) => {
   const foundSubscription = subscriptions.find(
     (subscription) => subscription.id.toString() === subscriptionId,
   );
-  res.send(foundSubscription);
+  res.send(!foundSubscription ? 'Subscription does not exist yet' : foundSubscription);
 });
 
 module.exports = router;
+
+// POST
+
+router.post('/', (req, res) => {
+  const newSubscription = req.body;
+  subscriptions.push(newSubscription);
+  fs.writeFile('src/data/subscription.json', JSON.stringify(subscriptions, null, 2), (err) => {
+    if (err) {
+      res.send('Error! Subscription cannot be created');
+    } else {
+      res.send('Subscription created!');
+    }
+  });
+});
+
+// DELETE
+
+router.delete('/:id', (req, res) => {
+  const subscriptionId = req.params.id;
+  const filteredSubscriptions = subscriptions.filter(
+    (subscription) => subscription.id.toString() !== subscriptionId,
+  );
+  fs.writeFile('src/data/subscription.json', JSON.stringify(filteredSubscriptions, null, 2), (err) => {
+    if (err) {
+      res.send('Error! Subscription cannot be deleted');
+    } else {
+      res.send('Subscription deleted!');
+    }
+  });
+});
