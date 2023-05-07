e23d8dd1ef71d4e5f8c22ed6d6dec96b58cbeaeb
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-05-07 14:34:58 -0300

    MR: PUT validations finished

--- src/resources/member.js
+++ src/resources/member.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const fs = require('fs');
 
+const router = express.Router();
 const members = require('../data/member.json');
 
-const router = express.Router();
-
 router.put('/:id', (req, res) => {
-  const found = members.some((member) => member.id.toString() === req.params.id);
-  if (found) {
-    const updateMember = req.body;
-    members.forEach((member) => {
-      if (member.id.toString() === req.params.id) {
-        /* eslint-disable no-param-reassign */
-        member.first_name = updateMember.first_name || member.first_name;
-        member.last_name = updateMember.last_name || member.last_name;
-        member.address = updateMember.address || member.address;
-        member.date = updateMember.date || member.date;
-        member.phone = updateMember.phone || member.phone;
-        member.email = updateMember.email || member.email;
-        member.password = updateMember.password || member.password;
-        member.membership = updateMember.membership || member.membership;
+  const memberFound = members.find((member) => member.id.toString() === req.params.id);
+  const updateMember = req.body;
 
-        res.status(200).json({ message: 'Member updated!', member });
-      }
-    });
-  } else {
+  if (!memberFound) {
     res.status(400).json({ message: `The member with the ID:${req.params.id} doesn't exist` });
+    return;
   }
+
+  if (Object.entries(updateMember).length === 0) {
+    res.status(400).json({ message: 'There has been no changes', memberFound });
+    return;
+  }
+
+  memberFound.first_name = updateMember.first_name || memberFound.first_name;
+  memberFound.last_name = updateMember.last_name || memberFound.last_name;
+  memberFound.address = updateMember.address || memberFound.address;
+  memberFound.date = updateMember.date || memberFound.date;
+  memberFound.phone = updateMember.phone || memberFound.phone;
+  memberFound.email = updateMember.email || memberFound.email;
+  memberFound.password = updateMember.password || memberFound.password;
+  memberFound.membership = updateMember.membership || memberFound.membership;
+
+  const memberIndex = members.indexOf(memberFound);
+  members[memberIndex] = memberFound;
+
+  fs.writeFile('src/data/member.json', JSON.stringify(members, null, 2), (err) => {
+    if (err) throw err;
+    res.status(200).json({ message: 'Member updated!', memberFound });
+  });
 });
 
 router.post('/', (req, res) => {
@@ -46,8 +53,8 @@ router.post('/', (req, res) => {
 
   const actualKeys = Object.keys(memberKeys);
   const newKeys = Object.keys(newMember);
-
   let equalKeys = true;
+
   for (let i = 0; i < actualKeys.length; i += 1) {
     if (actualKeys[i] !== newKeys[i]) {
       equalKeys = false;
@@ -55,6 +62,12 @@ router.post('/', (req, res) => {
   }
 
   newMember = { id: lastId + 1, ...newMember };
+
+  const memberExists = members.some((element) => element.email === newMember.email);
+
+  if (memberExists) {
+    res.status(400).json({ message: 'This member already exists' });
+  }
 
   if (Object.entries(newMember).length === 0 || !condition || !equalKeys) {
     res.status(400).json({ message: 'You must complete all the properties:', properties: `${Object.keys(memberKeys)}` });
@@ -64,12 +77,6 @@ router.post('/', (req, res) => {
       if (err) throw err;
       res.status(200).json({ message: 'User added successfuly!', members });
     });
-  }
-
-  const memberExists = members.some((element) => element.email === newMember.email);
-
-  if (memberExists) {
-    res.status(400).json({ message: 'This member already exists' });
   }
 });
 
