0525ef1a488b8c971ce27d7e8096b3c20b6d5f87
Federico Peralta <fedeper100@hotmail.com>
2023-05-07 21:25:02 -0300

    MR: PUT finished

--- src/resources/trainer.js
+++ src/resources/trainer.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const fs = require('fs');
 
 const trainers = require('../data/trainer.json');
 
 const router = express.Router();
 
+// GET all the trainers
+
 router.get('/', (req, res) => res.json(trainers));
+
+// GET
 
 router.get('/:id', (req, res) => {
   const trainerFound = trainers.find((trainer) => trainer.id.toString() === req.params.id);
@@ -15,4 +20,29 @@ router.get('/:id', (req, res) => {
   }
 });
 
+// PUT
+
+router.put('/:id', (req, res) => {
+  const trainerId = req.params.id;
+  const trainerFound = trainers.find((trainer) => trainer.id.toString() === trainerId);
+  const trainerUpd = req.body;
+  const indexFound = trainers.indexOf(trainerFound);
+  if (!trainerFound) {
+    res.status(400).json({ msg: `The trainer ${trainerId} doesn't exist` });
+  }
+  if (Object.entries(trainerUpd).length === 0) {
+    res.status(400).json({ msg: 'Please fill the empty fields' });
+  }
+  trainerFound.email = trainerUpd.email ? trainerUpd.email : trainerFound.email;
+  trainerFound.password = trainerUpd.password ? trainerUpd.password : trainerFound.password;
+  trainerFound.first_name = trainerUpd.first_name ? trainerUpd.first_name : trainerFound.first_name;
+  trainerFound.last_name = trainerUpd.last_name ? trainerUpd.last_name : trainerFound.last_name;
+  trainerFound.phone = trainerUpd.phone ? trainerUpd.phone : trainerFound.phone;
+  trainerFound.location = trainerUpd.location ? trainerUpd.location : trainerFound.location;
+  trainers[indexFound] = trainerFound;
+  fs.writeFile('src/data/trainer.json', JSON.stringify(trainers, null, 2), (err) => {
+    if (err) throw err;
+    res.status(200).json({ msg: `Trainer ${trainerId} has been modified`, trainerFound });
+  });
+});
 module.exports = router;
