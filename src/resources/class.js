b5c37992b5dcb62a6a7fc53a5555299e8719b290
ryaev <robert.yaev@gmail.com>
2023-05-07 21:52:01 -0300

    MR: method put is added

--- src/resources/class.js
+++ src/resources/class.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable max-len */
 const express = require('express');
 const fs = require('fs');
 const classes = require('../data/class.json');
@@ -23,9 +25,9 @@ router.post('/new', (req, res) => {
   classes.push(newClass);
   fs.writeFile('src/data/class.json', JSON.stringify(classes, null, 2), (err) => {
     if (err) {
-      res.send('User cannot be created');
+      res.send('Class cannot be created');
     } else {
-      res.send('User created successfully!');
+      res.send('Class created successfully!');
     }
   });
 });
@@ -37,13 +39,36 @@ router.delete('/:id', (req, res) => {
   classes.push(newClass);
   fs.writeFile('src/data/class.json', JSON.stringify(filteredClasses, null, 2), (err) => {
     if (err) {
-      res.send('User cannot be deleted');
+      res.send('Class cannot be deleted');
     } else {
-      res.send('User deleted!');
+      res.send('Class deleted!');
     }
   });
 });
 
-module.exports = router;
+router.put('/:id', (req, res) => {
+  const classId = req.params.id;
+  const foundClass = classes.find((classe) => classe.id.toString() === classId);
+  const updateClass = req.body;
+  if (foundClass) {
+    classes.forEach((classe) => {
+      if (classe.id.toString() === classId) {
+        classe.class_title = updateClass.class_title ? updateClass.class_title : classe.class_title;
+        classe.related_activity = updateClass.related_activity ? updateClass.related_activity : classe.related_activity;
+        classe.difficulty = updateClass.difficulty ? updateClass.difficulty : classe.difficulty;
+        classe.assigned_coach = updateClass.assigned_coach ? updateClass.assigned_coach : classe.assigned_coach;
+        classe.space_available = updateClass.space_available ? updateClass.space_available : classe.space_available;
+        classe.date = updateClass.date ? updateClass.date : classe.date;
+        classe.start_time = updateClass.start_time ? updateClass.start_time : classe.start_time;
+        classe.end_time = updateClass.end_time ? updateClass.end_time : classe.end_time;
+        classe.recurrence = updateClass.recurrence ? updateClass.recurrence : classe.recurrence;
 
-// console.log('Class JS');
+        res.json({ msg: 'Class edited successfully', classe });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No class winth that ID ${classId}` });
+  }
+});
+
+module.exports = router;
