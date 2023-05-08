798bd83373faf09ad03827bac28c7254f6a7895d
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2023-05-08 13:21:27 -0300

    MR: new filter options added

--- src/resources/member.js
+++ src/resources/member.js
@@ -7,19 +7,44 @@ const members = require('../data/member.json');
 router.use(express.urlencoded({ extended: false }));
 
 router.get('/', (req, res) => {
-  const { firstName } = req.query;
-  if (!firstName) {
+  const errorMessage = "it's not registered";
+
+  const { firstName, membership, email } = req.query;
+
+  if (firstName) {
+    const filteredName = members.filter((member) => member.first_name === firstName);
+    // eslint-disable-next-line no-unused-expressions
+    (filteredName.length === 0)
+      ? res.status(400).send(`${firstName} ${errorMessage}`)
+      : res.send(filteredName);
+  }
+
+  if (membership) {
+    const filteredMembership = members.filter((member) => member.membership === membership);
+    // eslint-disable-next-line no-unused-expressions
+    (filteredMembership.length === 0)
+      ? res.status(400).send(`${membership} is not a valid membership`)
+      : res.send(filteredMembership);
+  }
+
+  if (email) {
+    const filteredEmail = members.find((member) => member.email === email);
+    // eslint-disable-next-line no-unused-expressions
+    (!filteredEmail)
+      ? res.status(400).send(`${email} ${errorMessage}`)
+      : res.send(filteredEmail);
+  }
+
+  if (!firstName || !membership || !email) {
     res.send(members);
   }
-  const filteredMembers = members.filter((member) => member.first_name === firstName);
-  res.send(filteredMembers);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   const memberFound = members.find((member) => member.id.toString() === id);
   // eslint-disable-next-line no-unused-expressions
-  (memberFound) ? res.status(200).send(memberFound) : res.status(400).send("Member doesn't exists");
+  (memberFound) ? res.status(200).send(memberFound) : res.status(400).send("Member it's not registered");
 });
 
 router.delete('/:id', (req, res) => {
