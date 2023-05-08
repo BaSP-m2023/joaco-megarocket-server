82f8e133b07089e6d492fe2e929dd37d5554ce3c
ryaev <robert.yaev@gmail.com>
2023-05-08 19:01:26 -0300

    MR: POST method is modified

--- src/resources/class.js
+++ src/resources/class.js
@@ -20,15 +20,33 @@ router.get('/:id', (req, res) => {
   }
 });
 
+// eslint-disable-next-line consistent-return
 router.post('/new', (req, res) => {
-  const newClass = req.body;
+  const lastClassesId = classes[classes.length - 1].id;
+  const newClass = {
+    id: lastClassesId + 1,
+    class_title: req.body.class_title,
+    related_activity: req.body.related_activity,
+    difficulty: req.body.difficulty,
+    assigned_coach: req.body.assigned_coach,
+    space_available: req.body.space_available,
+    assigned_place: req.body.assigned_place,
+    date: req.body.date,
+    start_time: req.body.start_time,
+    end_time: req.body.end_time,
+    recurrence: req.body.recurrence,
+  };
+
+  if (!newClass.class_title || !newClass.related_activity || !newClass.difficulty || !newClass.assigned_coach
+    || !newClass.space_available || !newClass.assigned_place || !newClass.date || !newClass.start_time
+    || !newClass.end_time || !newClass.recurrence) {
+    return res.status(200).json({ msg: 'You must complete all the attributes to create a new class' });
+  }
+
   classes.push(newClass);
   fs.writeFile('src/data/class.json', JSON.stringify(classes, null, 2), (err) => {
-    if (err) {
-      res.send('Class cannot be created');
-    } else {
-      res.send('Class created successfully!');
-    }
+    if (err) throw err;
+    res.status(200).json({ msg: 'Class created successfully', newClass });
   });
 });
 
