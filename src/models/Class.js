1f4642f8dc409b34d64874f59ef9e4514556b737
JulianRiedinger7 <julianriedinger27@gmail.com>
2023-05-12 16:08:56 -0300

    MR-19: added class schema

--- src/models/Class.js
+++ src/models/Class.js
@@ -1,2 +1,35 @@
-// eslint-disable-next-line no-console
-console.log('test');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const classSchema = new Schema({
+  id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  day: {
+    type: String,
+    enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    required: true,
+  },
+  hour: {
+    type: String,
+    required: true,
+  },
+  trainer: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  activity: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  slots: {
+    type: Number,
+    default: 50,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Class', classSchema);
