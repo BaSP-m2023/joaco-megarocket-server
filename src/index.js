2d041f1b89022f1df41d74bd00fe349134830840
YhanahiRosenthal <yhanahi16@gmail.com>
2023-05-12 13:20:35 -0300

    MR-1: files name to PascalCase

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ mongoose.connect(url)
 
 const app = express();
 const port = process.env.PORT || 4002;
+// eslint-disable-next-line import/no-unresolved
 const router = require('./routes');
 
 app.use(cors());
