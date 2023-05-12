582a01b656b94a876a242ee574499b98cfb182b1
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-05-12 16:24:54 -0300

    MR-13: created Activity Schema

--- src/models/Activity.js
+++ src/models/Activity.js
@@ -1,2 +1,23 @@
-// eslint-disable-next-line no-console
-console.log('test');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const activitySchema = new Schema({
+  id: new mongoose.Types.ObjectId(),
+  name: {
+    type: String,
+    required: true,
+    minLenght: 3,
+  },
+  description: {
+    type: String,
+    minLenght: 40,
+    required: false,
+  },
+  is_active: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Activity', activitySchema);
