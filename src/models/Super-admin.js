4c90a779e36173392138ff753420793ff4021e76
YhanahiRosenthal <yhanahi16@gmail.com>
2023-05-12 15:07:03 -0300

    MR-2: Schema of super admin created

--- src/models/Super-admin.js
+++ src/models/Super-admin.js
@@ -1,2 +1,13 @@
-// eslint-disable-next-line no-console
-console.log('test');
+const { mongoose } = require('mongoose');
+
+const { Schema } = mongoose;
+
+const SuperAdminSchema = new Schema({
+
+  id: new mongoose.Types.ObjectId(),
+  email: String,
+  password: String,
+
+});
+
+module.export = mongoose.model('SuperAdmin', SuperAdminSchema);
