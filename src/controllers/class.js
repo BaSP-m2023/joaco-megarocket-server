0e585be7a2ca9d9467aef9f38dc038ef2e8616ac
JulianRiedinger7 <julianriedinger27@gmail.com>
2023-05-13 19:16:54 -0300

    MR-20: added DELETE router & controller & updated postman collection

--- src/controllers/class.js
+++ src/controllers/class.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Class = require('../models/Class');
 
 const createClass = (req, res) => {
@@ -23,6 +24,36 @@ const createClass = (req, res) => {
     }));
 };
 
+const deleteClass = (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({
+      message: `Format of ID ${id} is incorrect`,
+      data: undefined,
+      error: true,
+    });
+  }
+
+  return Class.findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          message: `Class with ID ${id} was not found`,
+          data: undefined,
+          error: true,
+        });
+      }
+      return res.status(204).end();
+    })
+    .catch((error) => res.status(400).json({
+      message: `An error ocurred: ${error}`,
+      data: undefined,
+      error: true,
+    }));
+};
+
 module.exports = {
   createClass,
+  deleteClass,
 };
--- src/routes/class.js
+++ src/routes/class.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { validateCreation } = require('../validations/class');
-const { createClass } = require('../controllers/class');
+const { createClass, deleteClass } = require('../controllers/class');
 
 const router = express.Router();
 
-router.post('/', validateCreation, createClass);
+router
+  .post('/', validateCreation, createClass)
+  .delete('/:id', deleteClass);
 
 module.exports = router;
