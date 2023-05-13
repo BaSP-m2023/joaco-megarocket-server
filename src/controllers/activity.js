5e0930562c95799016e50674f7184661aaae828f
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-05-13 16:38:48 -0300

    MR-14: getAll and getByID endpoints

--- src/controllers/activity.js
+++ src/controllers/activity.js
@@ -1,11 +1,50 @@
 const Activity = require('../models/Activity');
 
-const getAllActivities = () => {
-
+const getAllActivities = (req, res) => {
+  Activity.find()
+    .then((activities) => {
+      if (activities.length === 0) {
+        return res.status(404).json({
+          message: 'There are no activities',
+          data: undefined,
+          error: true,
+        });
+      }
+      return res.status(200).json({
+        message: 'Activities List',
+        data: activities,
+        error: false,
+      });
+    })
+    .catch((error) => res.status(500).json({
+      message: 'An error ocurred',
+      data: undefined,
+      error,
+    }));
 };
 
-const getActivityByID = () => {
-
+const getActivityByID = (req, res) => {
+  const { id } = req.params;
+  Activity.findById(id)
+    .then((activity) => {
+      if (!activity) {
+        return res.status(404).json({
+          message: 'Activity not found',
+          data: undefined,
+          error: true,
+        });
+      }
+      return res.status(200).json({
+        message: 'Activity found!',
+        data: activity,
+        error: false,
+      });
+    })
+    .catch((error) => res.status(500).json({
+      message: 'An error ocurred',
+      data: undefined,
+      error,
+    }));
 };
 
 const createActivity = () => {
