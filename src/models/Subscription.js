0a7015aa8467e57dd26e289408b3869f127057fe
Florencia Di Mónaco <flordimonaco@gmail.com>
2023-05-14 13:19:30 -0300

    MR-27: schema created

--- src/models/Subscription.js
+++ src/models/Subscription.js
@@ -1,2 +1,21 @@
-// eslint-disable-next-line no-console
-console.log('test');
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const subscriptionSchema = new Schema({
+  class: {
+    type: String,
+    required: true,
+    enum: ["Crossfit", "Box", "Functional", "Spinning", "Fitness"],
+  },
+  member: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.export = mongoose.model("Subscription", subscriptionSchema);
