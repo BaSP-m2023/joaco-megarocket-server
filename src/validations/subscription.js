bee65a1ed1c45955848c8f2497f32fe612cb5dbd
Florencia Di Mónaco <flordimonaco@gmail.com>
2023-05-16 18:35:47 -0300

    MR-27: validation update

--- src/validations/subscription.js
+++ src/validations/subscription.js
@@ -2,8 +2,8 @@ const Joi = require('joi');
 
 const createValidation = (req, res, next) => {
   const subscriptionValidation = Joi.object({
-    classes: Joi.isSchema().required(),
-    member: Joi.isSchema().required(),
+    classes: Joi.string().alphanum().length(24).required(),
+    member: Joi.string().alphanum().length(24).required(),
     date: Joi.date(),
   });
 
@@ -19,4 +19,23 @@ const createValidation = (req, res, next) => {
   }
 };
 
-module.exports = createValidation;
+const updateValidation = (req, res, next) => {
+  const subscriptionValidation = Joi.object({
+    classes: Joi.string().alphanum().length(24),
+    member: Joi.string().alphanum().length(24),
+    date: Joi.date(),
+  });
+
+  const validation = subscriptionValidation.validate(req.body);
+  if (validation.error) {
+    res.status(400).json({
+      error: true,
+      message: `Error: ${validation.error.details[0].message}`,
+      data: undefined,
+    });
+  } else {
+    next();
+  }
+};
+
+module.exports = { createValidation, updateValidation };
