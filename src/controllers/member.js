ca9d71c54e1bcae57f3bad8c3a4b4df3bff6bf89
Federico Peralta <fedeper100@hotmail.com>
2023-05-19 17:18:18 -0300

    MR-35: GetMembersById async finished

--- src/controllers/member.js
+++ src/controllers/member.js
@@ -24,34 +24,35 @@ const getAllMembers = async (req, res) => {
   }
 };
 
-const getMembersById = (req, res) => {
+const getMembersById = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.isValidObjectId(id)) {
-    res.status(400).json({
+    return res.status(400).json({
       message: 'Invalid id',
       data: id,
       error: true,
     });
-  } else {
-    Member.findById(id)
-      .then((member) => {
-        if (!member) {
-          return res.status(404).json({
-            message: 'Member not found',
-            error: true,
-          });
-        }
-        return res.status(200).json({
-          message: 'Member found',
-          data: member,
-          error: false,
-        });
-      })
-      .catch((error) => res.status(400).json({
-        message: `An error occurred with the ID: ${id}`,
-        data: error,
-      }));
+  }
+  try {
+    const findMemberById = await Member.findById(id);
+    if (!findMemberById) {
+      return res.status(404).json({
+        message: 'Member not found',
+        error: true,
+      });
+    }
+    return res.status(200).json({
+      message: 'Member found',
+      data: findMemberById,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      data: error,
+      error: true,
+    });
   }
 };
 
