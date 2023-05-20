a48f5344c7551b677dc82499e89eb59d2329182f
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-05-19 23:13:46 -0300

    BaSP-MR-36: code fixes and added ASYNC AWAIT to endpoints

--- src/controllers/activity.js
+++ src/controllers/activity.js
@@ -1,31 +1,35 @@
 const { default: mongoose } = require('mongoose');
 const Activity = require('../models/Activity');
 
-const getAllActivities = (req, res) => {
-  Activity.find()
-    .then((activities) => {
-      if (activities.length === 0) {
-        return res.status(404).json({
-          message: 'There are no activities',
-          data: activities,
-          error: false,
-        });
-      }
-      return res.status(200).json({
-        message: 'Activities List',
-        data: activities,
+const getAllActivities = async (req, res) => {
+  try {
+    const activitiesFound = await Activity.find();
+
+    if (activitiesFound.length === 0) {
+      return res.status(404).json({
+        message: 'There are no activities',
+        data: activitiesFound,
         error: false,
       });
-    })
-    .catch((error) => res.status(500).json({
+    }
+
+    return res.status(200).json({
+      message: 'Activities List',
+      data: activitiesFound,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
       message: `An error ocurred: ${error}`,
       data: undefined,
       error: true,
-    }));
+    });
+  }
 };
 
-const getActivityByID = (req, res) => {
+const getActivityByID = async (req, res) => {
   const { id } = req.params;
+
   if (!mongoose.isValidObjectId(id)) {
     return res.status(400).json({
       message: 'Invalid ID',
@@ -34,59 +38,68 @@ const getActivityByID = (req, res) => {
     });
   }
 
-  return Activity.findById(id)
-    .then((activity) => {
-      if (!activity) {
-        return res.status(404).json({
-          message: 'Activity not found',
-          data: undefined,
-          error: true,
-        });
-      }
-      return res.status(200).json({
-        message: 'Activity found!',
-        data: activity,
-        error: false,
+  try {
+    const activityFound = await Activity.findById(id);
+
+    if (!activityFound) {
+      return res.status(404).json({
+        message: 'Activity not found',
+        data: undefined,
+        error: true,
       });
-    })
-    .catch((error) => res.status(500).json({
+    }
+
+    return res.status(200).json({
+      message: 'Activity found!',
+      data: activityFound,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
       message: `An error ocurred: ${error}`,
       data: undefined,
       error: true,
-    }));
+    });
+  }
 };
 
 const createActivity = async (req, res) => {
   const { name, description, isActive } = req.body;
 
-  const activityExists = await Activity.findOne({ name });
-  if (activityExists) {
-    return res.status(400).json({
-      message: `${name} activity already exists`,
+  try {
+    const activityExists = await Activity.findOne({ name });
+
+    if (activityExists) {
+      return res.status(400).json({
+        message: `${name} activity already exists`,
+        data: undefined,
+        error: true,
+      });
+    }
+
+    const newActivity = await Activity.create({
+      name,
+      description,
+      isActive,
+    });
+
+    return res.status(201).json({
+      message: 'Activity created successfuly',
+      data: newActivity,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: `An error ocurred: ${error}`,
       data: undefined,
       error: true,
     });
   }
-
-  return Activity.create({
-    name,
-    description,
-    isActive,
-  })
-    .then((newActivity) => res.status(201).json({
-      message: 'Activity created successfuly',
-      data: newActivity,
-      error: false,
-    }))
-    .catch((error) => res.status(500).json({
-      message: `An error ocurred: ${error}`,
-      data: undefined,
-      error: true,
-    }));
 };
 
 const updateActivity = async (req, res) => {
   const { id } = req.params;
+
   if (!mongoose.isValidObjectId(id)) {
     return res.status(400).json({
       message: 'Invalid ID',
@@ -97,55 +110,59 @@ const updateActivity = async (req, res) => {
 
   const { name, description, isActive } = req.body;
 
-  const actualActivity = await Activity.findById(id);
-  const activityProperties = Object.keys(actualActivity.toObject()).slice(1, -1);
-  let changes = false;
-  activityProperties.forEach((property) => {
-    if (req.body[property]
-    && req.body[property].toString() !== actualActivity[property].toString()) {
-      changes = true;
+  try {
+    const actualActivity = await Activity.findById(id);
+
+    if (!actualActivity) {
+      return res.status(404).json({
+        message: `The activity with the ID: ${id} doesn't exists`,
+        data: undefined,
+        error: true,
+      });
     }
-  });
 
-  if (!changes) {
-    return res.status(200).json({
-      message: 'There were no changes',
-      data: actualActivity,
-      error: false,
-    });
-  }
-
-  return Activity.findByIdAndUpdate(
-    id,
-    {
-      name,
-      description,
-      isActive,
-    },
-    { new: true },
-  )
-    .then((activity) => {
-      if (!activity) {
-        return res.status(404).json({
-          message: `The activity with the ID: ${activity.id} doesn't exists`,
-          data: undefined,
-          error: true,
-        });
+    const activityProperties = Object.keys(actualActivity.toObject()).slice(1, -1);
+    let changes = false;
+    activityProperties.forEach((property) => {
+      if (req.body[property]
+      && req.body[property].toString().toLowerCase() !== actualActivity[property].toString()) {
+        changes = true;
       }
+    });
+
+    if (!changes) {
       return res.status(200).json({
-        message: 'Activity updated successfuly!',
-        data: activity,
+        message: 'There were no changes',
+        data: actualActivity,
         error: false,
       });
-    })
-    .catch((error) => res.status(500).json({
+    }
+
+    const activityUpdated = await Activity.findByIdAndUpdate(
+      id,
+      {
+        name,
+        description,
+        isActive,
+      },
+      { new: true },
+    );
+
+    return res.status(200).json({
+      message: 'Activity updated successfuly!',
+      data: activityUpdated,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
       message: `An error ocurred: ${error}`,
       data: undefined,
       error: true,
-    }));
+    });
+  }
 };
 
-const deleteActivity = (req, res) => {
+const deleteActivity = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.isValidObjectId(id)) {
@@ -156,22 +173,29 @@ const deleteActivity = (req, res) => {
     });
   }
 
-  return Activity.findByIdAndDelete(id)
-    .then((activity) => {
-      if (!activity) {
-        return res.status(404).json({
-          message: 'Activity not found',
-          data: undefined,
-          error: true,
-        });
-      }
-      return res.status(204).json({});
-    })
-    .catch((error) => res.status(500).json({
+  try {
+    const activityDeleted = await Activity.findByIdAndDelete(id);
+
+    if (!activityDeleted) {
+      return res.status(404).json({
+        message: 'Activity not found',
+        data: undefined,
+        error: true,
+      });
+    }
+
+    return res.status(200).json({
+      message: 'Activity deleted successfuly!',
+      data: activityDeleted,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
       message: `An error ocurred: ${error}`,
       data: undefined,
       error: true,
-    }));
+    });
+  }
 };
 
 module.exports = {
