a320340b6f7e1f6ffc739b4354613e20b372f5ee
Eugenio Weskamp <e.weskamp8@gmail.com>
2023-05-20 14:34:24 -0300

    MR-46: code fixes

--- src/controllers/trainer.js
+++ src/controllers/trainer.js
@@ -1,53 +1,19 @@
 const { default: mongoose } = require('mongoose');
 const Trainer = require('../models/Trainer');
 
-const createTrainer = (req, res) => {
+const createTrainer = async (req, res) => {
   const {
     firstName, lastName, dni, phone, email, city, password, salary, isActive,
   } = req.body;
 
-  Trainer.create({
-    firstName,
-    lastName,
-    dni,
-    phone,
-    email,
-    city,
-    password,
-    salary,
-    isActive,
-  })
+  try {
+    const found = await Trainer.findOne({ $or: [{ dni }, { email }] });
 
-    .then((newTrainer) => res.status(201).json({
-      message: 'Trainer created successfuly',
-      data: newTrainer,
-      error: false,
-    }))
+    if (found) {
+      throw Error(`The admin with DNI ${dni} or Email ${email} already exists`);
+    }
 
-    .catch((error) => res.status(400).json({
-      message: 'Failed to create a Trainer',
-      error,
-    }));
-};
-
-const updateTrainer = (req, res) => {
-  const { id } = req.params;
-
-  if (!mongoose.isValidObjectId(id)) {
-    return res.status(400).json({
-      message: 'Invalid ID',
-      data: id,
-      error: true,
-    });
-  }
-
-  const {
-    firstName, lastName, dni, phone, email, city, password, salary, isActive,
-  } = req.body;
-
-  return Trainer.findByIdAndUpdate(
-    id,
-    {
+    const newTrainer = await Trainer.create({
       firstName,
       lastName,
       dni,
@@ -57,31 +23,31 @@ const updateTrainer = (req, res) => {
       password,
       salary,
       isActive,
-    },
-    { new: true },
-  )
-    .then((trainer) => {
-      if (!trainer) {
-        return res.status(404).json({
-          message: `Trainer with the id: ${id} was not fount`,
-          data: undefined,
-          error: true,
-        });
-      }
-      return res.status(201).json({
-        message: 'Trainer updated succesfully',
-        data: trainer,
-        error: false,
+    });
+
+    return res.status(201).json({
+      message: 'Trainer created successfuly',
+      data: newTrainer,
+      error: false,
+    });
+  } catch (error) {
+    if (error.message === `The admin with DNI ${dni} or Email ${email} already exists`) {
+      return res.status(400).json({
+        message: error.message,
+        data: undefined,
+        error: true,
       });
-    })
-    .catch((error) => res.status(400).json({
-      message: 'An error ocurred',
+    }
+
+    return res.status(500).json({
+      message: error.message,
       data: undefined,
-      error,
-    }));
+      error: true,
+    });
+  }
 };
 
-const deleteTrainer = (req, res) => {
+const updateTrainer = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.isValidObjectId(id)) {
@@ -92,24 +58,84 @@ const deleteTrainer = (req, res) => {
     });
   }
 
-  return Trainer.findByIdAndDelete(id)
-    .then((result) => {
-      if (!result) {
-        return res.status(404).json({
-          message: `Trainer with ID: ${id} was not found`,
-          data: undefined,
-          error: true,
-        });
-      }
-      return res.status(204).json({
-        message: 'Trainer success deleted!',
+  const {
+    firstName, lastName, dni, phone, email, city, password, salary, isActive,
+  } = req.body;
+
+  try {
+    const actualTrainer = await Trainer.findById(id);
+
+    if (!actualTrainer) {
+      return res.status(404).json({
+        message: `The trainer with the ID: ${id} doesn't exists`,
+        data: undefined,
+        error: true,
       });
-    })
-    .catch((error) => res.status(400).json({
-      message: 'An error ocurred',
+    }
+
+    const trainerUpdate = await Trainer.findByIdAndUpdate(
+      id,
+      {
+        firstName,
+        lastName,
+        dni,
+        phone,
+        email,
+        city,
+        password,
+        salary,
+        isActive,
+      },
+      { new: true },
+    );
+    return res.status(201).json({
+      message: 'Trainer updated succesfully',
+      data: trainerUpdate,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: `An error ocurred: ${error}`,
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
+const deleteTrainer = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({
+      message: 'Invalid ID',
+      data: id,
+      error: true,
+    });
+  }
+
+  try {
+    const trainerDelete = await Trainer.findByIdAndDelete(id);
+
+    if (!trainerDelete) {
+      return res.status(404).json({
+        message: `Trainer with ID: ${id} was not found`,
+        data: undefined,
+        error: true,
+      });
+    }
+
+    return res.status(200).json({
+      message: 'Trainer successfully deleted!',
+      data: trainerDelete,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'An error occurred',
       data: undefined,
       error,
-    }));
+    });
+  }
 };
 
 const getAllTrainer = (req, res) => {
--- src/validations/trainer.js
+++ src/validations/trainer.js
@@ -1,15 +1,50 @@
 const Joi = require('joi');
 
+const RGXPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
 const validateCreation = (req, res, next) => {
   const trainerValidation = Joi.object({
     firstName: Joi.string().min(3).max(11).required(),
     lastName: Joi.string().min(3).max(30).required(),
-    dni: Joi.number().min(7).required(),
-    phone: Joi.number().min(10).required(),
+    dni: Joi.number().min(1000000).max(99999999).required(),
+    phone: Joi.number().min(1000000000).max(9999999999).required(),
     email: Joi.string().email().required(),
     city: Joi.string().min(3).required(),
-    password: Joi.string().required(),
+    password: Joi.string().regex(RGXPass)
+      .min(8)
+      .regex(RGXPass)
+      .required()
+      .messages({
+        'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, and one digit',
+      }),
     salary: Joi.number().required(),
+    isActive: Joi.boolean(),
+  });
+
+  const validation = trainerValidation.validate(req.body);
+  if (!validation.error) return next();
+  return res.status(400).json({
+    message: `There was an error: ${validation.error.details[0].message}`,
+    data: undefined,
+    error: true,
+  });
+};
+
+const validateUpdate = (req, res, next) => {
+  const trainerValidation = Joi.object({
+    firstName: Joi.string().min(3).max(11),
+    lastName: Joi.string().min(3).max(30),
+    dni: Joi.number().min(1000000).max(99999999),
+    phone: Joi.number().min(1000000000).max(9999999999),
+    email: Joi.string().email(),
+    city: Joi.string().min(3),
+    password: Joi.string().regex(RGXPass)
+      .min(8)
+      .regex(RGXPass)
+      .messages({
+        'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, and one digit',
+      }),
+    salary: Joi.number(),
     isActive: Joi.boolean(),
   });
 
@@ -24,4 +59,5 @@ const validateCreation = (req, res, next) => {
 
 module.exports = {
   validateCreation,
+  validateUpdate,
 };
