e1ded6fdcc5dbc6ff5dd13f7b63c344c5384c8f4
YhanahiRosenthal <yhanahi16@gmail.com>
2023-05-21 18:05:03 -0300

    MR-43: get test modified

--- src/seeds/activities.js
+++ src/seeds/activities.js
@@ -6,20 +6,17 @@ export default [
     name: 'box',
     description: 'basic training that consists of throwing soft punches into the air wit',
     isActive: true,
-    __v: 0,
   },
   {
     _id: new mongoose.Types.ObjectId('6466b3e7a21fd4069bcaf7c0'),
     name: 'yoga',
     description: 'helps improving your body and mental health',
     isActive: true,
-    __v: 0,
   },
   {
     _id: new mongoose.Types.ObjectId('6467cd965eada13a19071ab9'),
     name: 'functional',
     description: 'this activity involves movements that enable greater overall body functioning and improve performance through better coordination and muscle stimulation',
     isActive: false,
-    __v: 0,
   },
 ];
--- src/tests/activities.test.js
+++ src/tests/activities.test.js
@@ -5,24 +5,23 @@ import activitySeed from '../seeds/activities';
 
 beforeAll(async () => {
   await activity.collection.insertMany(activitySeed);
-}, 60000);
+});
 
 describe('GET api/activities', () => {
-  test('Should return all activites with status 200', async () => {
-    const response = await request(app).get('/activities').send();
+  test('Should return status 200', async () => {
+    const response = await request(app).get('/api/activities').send();
     expect(response.status).toBe(200);
     expect(response.error).toBeFalsy();
   });
 
-  // test('Should return status 404', async () => {
-  //   const response = await request(app).get('/activities').send();
-  //   expect(response.status).toBe(404);
-  //   expect(response.error).toBeTruthy();
-  // });
+  test('Should return status 404 if the endpoint no exist', async () => {
+    const response = await request(app).get('/api/activitie').send();
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  });
 
-  // test('Should return status 404 if the endpoint no exist', async () => {
-  //   const response = await request(app).get('/activitie').send();
-  //   expect(response.status).toBe(404);
-  //   expect(response.error).toBeTruthy();
-  // });
+  test("should return a activity", async () => {
+    const res = await request(app).get('/api/activities/6462261d7ead90b46b7471cc');
+    expect(res.status).toBe(200);
+  });
 });
