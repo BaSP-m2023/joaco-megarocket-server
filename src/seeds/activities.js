1d53a508396feff2d9654a7f56d610b5dff2fabc
YhanahiRosenthal <yhanahi16@gmail.com>
2023-05-21 16:22:53 -0300

    MR-43: test get

--- src/seeds/activities.js
+++ src/seeds/activities.js
@@ -1,1 +1,25 @@
-// test comment
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('6462261d7ead90b46b7471cc'),
+    name: 'box',
+    description: 'basic training that consists of throwing soft punches into the air wit',
+    isActive: true,
+    __v: 0,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6466b3e7a21fd4069bcaf7c0'),
+    name: 'yoga',
+    description: 'helps improving your body and mental health',
+    isActive: true,
+    __v: 0,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467cd965eada13a19071ab9'),
+    name: 'functional',
+    description: 'this activity involves movements that enable greater overall body functioning and improve performance through better coordination and muscle stimulation',
+    isActive: false,
+    __v: 0,
+  },
+];
--- src/tests/activities.test.js
+++ src/tests/activities.test.js
@@ -1,3 +1,28 @@
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import activity from '../models/Activity';
+import activitySeed from '../seeds/activities';
+
+beforeAll(async () => {
+  await activity.collection.insertMany(activitySeed);
+}, 60000);
+
+describe('GET api/activities', () => {
+  test('Should return all activites with status 200', async () => {
+    const response = await request(app).get('/activities').send();
+    expect(response.status).toBe(200);
+    expect(response.error).toBeFalsy();
+  });
+
+  // test('Should return status 404', async () => {
+  //   const response = await request(app).get('/activities').send();
+  //   expect(response.status).toBe(404);
+  //   expect(response.error).toBeTruthy();
+  // });
+
+  // test('Should return status 404 if the endpoint no exist', async () => {
+  //   const response = await request(app).get('/activitie').send();
+  //   expect(response.status).toBe(404);
+  //   expect(response.error).toBeTruthy();
+  // });
 });
