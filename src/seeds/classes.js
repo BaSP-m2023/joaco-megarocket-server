f8319c373a3703efeadde55a81b95ce5353264c5
Florencia Di Mónaco <flordimonaco@gmail.com>
2023-05-22 16:33:20 -0300

    MR-38: seeds data added

--- src/seeds/classes.js
+++ src/seeds/classes.js
@@ -1,1 +1,31 @@
-// test comment
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('64600b11c51d8d472e32c678'),
+    day: 'Sunday',
+    hour: '20:00',
+    slots: 8,
+    createdAt: '2023-05-13T22:11:29.657Z',
+    updatedAt: '2023-05-19T00:39:27.205Z',
+    __v: 0,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64622fe64f4dc6cfd285155b'),
+    day: 'Thursday',
+    hour: '17:00',
+    slots: 6,
+    createdAt: '2023-05-15T13:13:10.284Z',
+    updatedAt: '2023-05-15T13:13:10.284Z',
+    __v: 0,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6462359679333a385c67f703'),
+    day: 'Monday',
+    hour: '13:30',
+    slots: 7,
+    createdAt: '2023-05-15T13:37:26.139Z',
+    updatedAt: '2023-05-15T13:37:26.139Z',
+    __v: 0,
+  },
+];
