ea61b8c2de2b0938e91ca371bbe1bc8113ff1e6a
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-05-21 23:39:21 -0300

    MR-40: trainers seed

--- src/seeds/trainers.js
+++ src/seeds/trainers.js
@@ -1,1 +1,28 @@
-// test comment
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('6462a2ee2aac39ef714f13d5'),
+    firstName: 'Delfi',
+    lastName: 'jiji',
+    dni: 43576575,
+    phone: 3416534443,
+    email: 'delfi@gmail.com',
+    city: 'Rosario',
+    password: 'holis123aa',
+    salary: 50000,
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('646642acfac4c6a035b35000'),
+    firstName: 'Gino',
+    lastName: 'juju',
+    dni: 1234567,
+    phone: 1234567891,
+    email: 'gino@gmail.com',
+    city: 'Rosario',
+    password: 'abcdefg123',
+    salary: 60000,
+    isActive: false,
+  },
+];
