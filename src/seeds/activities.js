d515a769f84fc234b94d076cb80f8ae522cac94c
Florencia Di Mónaco <flordimonaco@gmail.com>
2023-05-23 18:06:50 -0300

    MR-38: made changes so there is no conflict

--- src/seeds/activities.js
+++ src/seeds/activities.js
@@ -9,4 +9,16 @@ export default [
     isActive: true,
     __v: 0,
   },
+  {
+    _id: new mongoose.Types.ObjectId('6466b3e7a21fd4069bcaf7c0'),
+    name: 'yoga',
+    description: 'helps improving your body and mental health',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467cd965eada13a19071ab9'),
+    name: 'functional',
+    description: 'this activity involves movements that enable greater overall body functioning and improve performance through better coordination and muscle stimulation',
+    isActive: false,
+  },
 ];
