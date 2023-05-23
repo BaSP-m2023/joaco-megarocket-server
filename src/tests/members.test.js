e43a03376401615d056e974a03766c0c3cf1501d
Federico Peralta <fedeper100@hotmail.com>
2023-05-22 23:28:38 -0300

    MR-34: PUT tests finished

--- src/tests/members.test.js
+++ src/tests/members.test.js
@@ -55,3 +55,61 @@ describe('GET /api/members/:id', () => {
     expect(response.body.error).toBeFalsy();
   });
 });
+
+describe('PUT /api/members/:id', () => {
+  test('If the ID is not valid return 400', async () => {
+    const id = '8514';
+    const response = await request(app).get(`/api/members/${id}`).send();
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.error).toBeTruthy();
+  });
+
+  test('If the ID is not found return 404', async () => {
+    const id = '6462d0074441252c694312dd';
+    const response = await request(app).get(`/api/members/${id}`).send();
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.error).toBeTruthy();
+  });
+
+  test('If dni has letters return 400', async () => {
+    const id = '64625f36956fb774c56da13e';
+    const reqBody = {
+      dni: 'eooo',
+    };
+    const response = await request(app).put(`/api/members/${id}`).send(reqBody);
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.error).toBeTruthy();
+  });
+
+  test('If update success return 201', async () => {
+    const id = '64625f36956fb774c56da13e';
+    const reqBody = {
+      firstName: 'Jorge',
+      lastName: 'Ruiz',
+      city: 'Rosario',
+    };
+    const response = await request(app).put(`/api/members/${id}`).send(reqBody);
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.data).toBeDefined();
+    expect(response.body.error).toBeFalsy();
+  });
+
+  test('If ID is trying to be updated return 400', async () => {
+    const id = '64625f36956fb774c56da13e';
+    const reqBody = {
+      _id: 'asdqawawdadw',
+    };
+    const response = await request(app).put(`/api/members/${id}`).send(reqBody);
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.error).toBeTruthy();
+  });
+});
