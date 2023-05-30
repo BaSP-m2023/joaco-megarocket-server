5438066585c01cd911822e48fb0dda719f8bb4b1
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-05-30 11:29:22 -0300

    app.listen now waits for mongoose conection

--- src/index.js
+++ src/index.js
@@ -4,23 +4,25 @@ import app from './app';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_DB_CONNECT_URL)
-// eslint-disable-next-line no-console
-  .then(() => console.log('MongoDB connected'))
-// eslint-disable-next-line no-console
+mongoose.connect(process.env.DB_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('MongoDB connected');
+
+    try {
+      app.listen(process.env.PORT, (err) => {
+        if (err) throw err;
+        // eslint-disable-next-line no-console
+        console.log(`server listening on port: ${process.env.PORT}`);
+      });
+    } catch (err) {
+    // eslint-disable-next-line no-console
+      console.log('There was an error starting the server: ', err);
+    }
+  })
+  // eslint-disable-next-line no-console
   .catch((e) => console.log(e));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-try {
-  app.listen(process.env.PORT, (err) => {
-    if (err) throw err;
-    // eslint-disable-next-line no-console
-    console.log(`server listening on port: ${process.env.PORT}`);
-  });
-} catch (err) {
-  // eslint-disable-next-line no-console
-  console.log('There was an error starting the server: ', err);
-}
