6cada55f02f63a06b4252f9b39d3f506015acf1d
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-06-26 00:16:48 -0300

    MR-161: Created auth route

---
+++ src/routes/auth.js
@@ -0,0 +1,48 @@
+import SuperAdmin from '../models/Super-admin';
+import Member from '../models/Member';
+import Admin from '../models/Admin';
+
+const getAuth = async (req, res) => {
+  try {
+    const member = await Member.findOne({ firebaseUid: req.headers.firebaseUid });
+    if (member) {
+      return res.status(201).json({
+        message: 'Member found',
+        data: member,
+        error: false,
+      });
+    }
+
+    const admin = await Admin.findOne({ firebaseUid: req.headers.firebaseUid });
+    if (admin) {
+      return res.status(201).json({
+        message: 'Admin found',
+        data: admin,
+        error: false,
+      });
+    }
+
+    const superAdmin = await SuperAdmin.findOne({ firebaseUid: req.headers.firebaseUid });
+    if (superAdmin) {
+      return res.status(201).json({
+        message: 'Super Admin found',
+        data: superAdmin,
+        error: false,
+      });
+    }
+
+    return res.status(404).json({
+      message: 'User not found',
+      data: undefined,
+      error: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Error',
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
+export default getAuth;
