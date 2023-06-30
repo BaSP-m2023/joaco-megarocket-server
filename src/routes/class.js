77f3561ec0d18264cdf000c1d5e6e6a7c9da1c0a
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-06-30 12:22:21 -0300

    MR-161: finished initial set-up

--- src/routes/class.js
+++ src/routes/class.js
@@ -6,8 +6,8 @@ import verifyToken from '../middleware/authMiddleware';
 const router = express.Router();
 
 router
-  .get('/', (req, res, next) => verifyToken(req, res, ['ADMIN', 'TRAINER', 'MEMBER'], next), classController.getAllClasses)
-  .get('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN', 'TRAINER', 'MEMBER'], next), classController.getClassesByID)
+  .get('/', classController.getAllClasses)
+  .get('/:id', classController.getClassesByID)
   .post('/', (req, res, next) => verifyToken(req, res, ['ADMIN'], next), validations.validateCreation, classController.createClass)
   .delete('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN'], next), classController.deleteClass)
   .put('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN'], next), validations.validateUpdate, classController.updateClass);
--- src/routes/subscription.js
+++ src/routes/subscription.js
@@ -8,8 +8,8 @@ const router = express.Router();
 router
   .get('/', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), subscriptionController.getSubscriptions)
   .get('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), subscriptionController.getSubscriptionsByID)
-  .post('/', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), validations.createValidation, subscriptionController.createSubscription)
-  .put('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN'], next), validations.updateValidation, subscriptionController.updateSubscription)
+  .post('/', (req, res, next) => verifyToken(req, res, ['MEMBER'], next), validations.createValidation, subscriptionController.createSubscription)
+  .put('/:id', (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN']/* SUPER_ADMIN is here because nobody can do this put */, next), validations.updateValidation, subscriptionController.updateSubscription)
   .delete('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), subscriptionController.deleteSubscription)
   .delete('/', subscriptionController.deleteOldSubscriptions);
 
