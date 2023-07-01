88043aa6a71ab537c7bd6111ad1077de49c5f326
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-07-01 13:25:19 -0300

    MR-186: commented some lines

--- src/routes/member.js
+++ src/routes/member.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import memberController from '../controllers/member';
 import validations from '../validations/member';
-import verifyToken from '../middleware/authMiddleware';
+/* import verifyToken from '../middleware/authMiddleware'; */
 
 const router = express.Router();
 
 router
-  .get('/', (req, res, next) => verifyToken(req, res, ['ADMIN'], next), memberController.getAllMembers)
-  .get('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER', 'TRAINER'], next), memberController.getMembersById)
-  .post('/', (req, res, next) => verifyToken(req, res, ['MEMBER'], next), validations.validateCreation, memberController.createMember)
-  .put('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), validations.validateUpdate, memberController.editMember)
-  .delete('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), memberController.deleteMember);
+  .get('/', /* (req, res, next) => verifyToken(req, res, ['ADMIN'], next), */ memberController.getAllMembers)
+  .get('/:id', /* (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER', 'TRAINER'], next), */ memberController.getMembersById)
+  .post('/', /* (req, res, next) => verifyToken(req, res, ['MEMBER'], next), */ validations.validateCreation, memberController.createMember)
+  .put('/:id', /* (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), */ validations.validateUpdate, memberController.editMember)
+  .delete('/:id', /* (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), */ memberController.deleteMember);
 
 export default router;
--- src/routes/subscription.js
+++ src/routes/subscription.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import subscriptionController from '../controllers/subscription';
 import validations from '../validations/subscription';
-import verifyToken from '../middleware/authMiddleware';
+/* import verifyToken from '../middleware/authMiddleware'; */
 
 const router = express.Router();
 
 router
-  .get('/', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), subscriptionController.getSubscriptions)
-  .get('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), subscriptionController.getSubscriptionsByID)
-  .post('/', (req, res, next) => verifyToken(req, res, ['MEMBER'], next), validations.createValidation, subscriptionController.createSubscription)
-  .put('/:id', (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN']/* SUPER_ADMIN is here because nobody can do this put */, next), validations.updateValidation, subscriptionController.updateSubscription)
-  .delete('/:id', (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), subscriptionController.deleteSubscription)
+  .get('/', /* (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), */ subscriptionController.getSubscriptions)
+  .get('/:id', /*  (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), */ subscriptionController.getSubscriptionsByID)
+  .post('/', /* (req, res, next) => verifyToken(req, res, ['MEMBER'], next), */ validations.createValidation, subscriptionController.createSubscription)
+  .put('/:id', /* (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN'] *//* SUPER_ADMIN is here because nobody can do this put *//* , next), */ validations.updateValidation, subscriptionController.updateSubscription)
+  .delete('/:id', /* (req, res, next) => verifyToken(req, res, ['ADMIN', 'MEMBER'], next), */ subscriptionController.deleteSubscription)
   .delete('/', subscriptionController.deleteOldSubscriptions);
 
 export default router;
